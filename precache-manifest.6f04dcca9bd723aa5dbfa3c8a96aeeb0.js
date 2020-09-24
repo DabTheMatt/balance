4e9579bbe24fbcf3829babed3ceadc3c06fa5236
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-09-24 11:41:14 +0200

    Updates

---
+++ precache-manifest.6f04dcca9bd723aa5dbfa3c8a96aeeb0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9131773b3894cb928a9e85dca569be53",
+    "url": "/balance/index.html"
+  },
+  {
+    "revision": "ed61b76e10bbc85649af",
+    "url": "/balance/static/css/main.7f74cabd.chunk.css"
+  },
+  {
+    "revision": "b52252883b2042109ee5",
+    "url": "/balance/static/js/2.24b5735f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/balance/static/js/2.24b5735f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed61b76e10bbc85649af",
+    "url": "/balance/static/js/main.3ddf804e.chunk.js"
+  },
+  {
+    "revision": "e0d70038a2ad4ae38ecd",
+    "url": "/balance/static/js/runtime-main.a3953208.js"
+  },
+  {
+    "revision": "1d4b3159af137c6cea123c526a1141be",
+    "url": "/balance/static/media/leg.1d4b3159.png"
+  },
+  {
+    "revision": "433c6c1fcc487867ded9f8fe8e101f20",
+    "url": "/balance/static/media/shelter.433c6c1f.png"
+  },
+  {
+    "revision": "a4be31ebab95384d1364571362e06705",
+    "url": "/balance/static/media/tree.a4be31eb.png"
+  },
+  {
+    "revision": "92b66916fa667043a0a58c8c316f4097",
+    "url": "/balance/static/media/vacine.92b66916.png"
+  }
+]);
