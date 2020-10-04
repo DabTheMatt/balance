d2bd86bc5b3852ac9d2b9aa32337f3963d22373e
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-10-04 16:03:26 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/balance/precache-manifest.3446800ee345f8832d2dcccdeafdafd4.js"
+  "/balance/precache-manifest.8d033059509c136cf3c19623bf65f8ce.js"
 );
 
 self.addEventListener('message', (event) => {
