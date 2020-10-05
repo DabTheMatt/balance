241a5f92894419bf7c6e6eecae5821598e61fbf9
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-10-05 15:05:22 +0200

    Updates

--- precache-manifest.51969527f628313b640b325041f1b458.js
+++ precache-manifest.51969527f628313b640b325041f1b458.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91ffe1c1c9ab67f903d98d59e57b8dbd",
+    "revision": "0fc5654ff2e037447843995d63351cb8",
     "url": "/balance/index.html"
   },
   {
-    "revision": "412d3092f3d37bba1a2e",
+    "revision": "eb8027d90b4065c692bd",
     "url": "/balance/static/css/main.8aab141f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/balance/static/js/2.81dfd91e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "412d3092f3d37bba1a2e",
-    "url": "/balance/static/js/main.3e838843.chunk.js"
+    "revision": "eb8027d90b4065c692bd",
+    "url": "/balance/static/js/main.5e1bdc0f.chunk.js"
   },
   {
     "revision": "e0d70038a2ad4ae38ecd",
