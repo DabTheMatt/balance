eb9e90a3c505cd68636fe8987a086138de328dda
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-10-08 17:50:42 +0200

    Updates

--- precache-manifest.4737b460816bb963b613456c82d6f881.js
+++ precache-manifest.4737b460816bb963b613456c82d6f881.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d669c02a79f69a26db408a2043180a6",
+    "revision": "5c3f95c2e8230785fdfbde3cb937d192",
     "url": "/balance/index.html"
   },
   {
-    "revision": "26da10a59c29af99ea39",
-    "url": "/balance/static/css/main.bfd35aa3.chunk.css"
+    "revision": "c12e4079483c65743935",
+    "url": "/balance/static/css/main.ddf7214b.chunk.css"
   },
   {
-    "revision": "439746bf3bca0fea09bf",
-    "url": "/balance/static/js/2.164a385a.chunk.js"
+    "revision": "bacd95cead05940dd56e",
+    "url": "/balance/static/js/2.0d457a6e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/balance/static/js/2.164a385a.chunk.js.LICENSE.txt"
+    "url": "/balance/static/js/2.0d457a6e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "26da10a59c29af99ea39",
-    "url": "/balance/static/js/main.c0f2f44d.chunk.js"
+    "revision": "c12e4079483c65743935",
+    "url": "/balance/static/js/main.9309e01f.chunk.js"
   },
   {
     "revision": "e0d70038a2ad4ae38ecd",
